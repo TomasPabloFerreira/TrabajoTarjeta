5d508379ff88852d547f093714c76699078633ec
TomasPabloFerreira <tomasferreira373@gmail.com>
2019-12-03 03:35:54 -0300

    Formating code

--- tests/BoletoTest.php
+++ tests/BoletoTest.php
@@ -4,21 +4,22 @@ namespace TrabajoTarjeta;
 
 use PHPUnit\Framework\TestCase;
 
-class BoletoTest extends TestCase {
-
-    public function testSaldoCero() {
+class BoletoTest extends TestCase
+{
+    public function testSaldoCero()
+    {
         $valor = Tarifas::BOLETO_NORMAL;
-		$tarjeta= new Tarjeta(2345);
-        $colectivo= new Colectivo(144,"RosarioBus",7);
-        $tiempo= new TiempoFalso;
+        $tarjeta = new Tarjeta(2345);
+        $colectivo = new Colectivo(144, "RosarioBus", 7);
+        $tiempo = new TiempoFalso;
         $tiempo->avanzar(1535563521);
-        $fecha= date("D/m/Y H:i:s", $tiempo->time());
+        $fecha = date("D/m/Y H:i:s", $tiempo->time());
         $boleto = new Boleto($valor, $colectivo, $tarjeta, $tiempo);
 
         $this->assertEquals($boleto->obtenerValor(), $valor);
         $this->assertEquals($boleto->obtenerLinea(), 144);
         $this->assertEquals($boleto->obtenerID(), 2345);
-        $this->assertEquals($boleto->obtenerFecha(),$fecha);
-        $this->assertEquals($boleto->obtenerSaldo(), 'Su saldo: '.$tarjeta->obtenerSaldo());
+        $this->assertEquals($boleto->obtenerFecha(), $fecha);
+        $this->assertEquals($boleto->obtenerSaldo(), 'Su saldo: ' . $tarjeta->obtenerSaldo());
     }
 }
