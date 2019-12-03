f849b12d57ac6f04222af86795fee0b2f74e4811
TomasPabloFerreira <tomasferreira373@gmail.com>
2019-12-03 01:56:53 -0300

    Test

--- src/Tarjeta.php
+++ src/Tarjeta.php
@@ -51,9 +51,47 @@ class Tarjeta implements TarjetaInterface
         return $this->saldo;
     }
 
+    public function trasbordo(TiempoInterface $tiempo, ColectivoInterface $colectivo)
+    {
+
+        if ($this->lineaUltColectivo != $colectivo->linea() && $this->cantTrasb == 0) {
+            $trasbordo = false;
+            $tiempoTranscurrido = $tiempo->time() - $this->ultimopago;
+
+            // Tiempo máximo 60 minutos.
+            if ($tiempoTranscurrido <= 3600) {
+                $trasbordo = true;
+            }
+
+            // Sábados de las 14 a 22 hs, domingos y feriados de 6 a 22 hs: tiempo máximo 90 minutos.
+            if ($tiempoTranscurrido <= 5400) {
+                $dia = date("l", $tiempo->time());
+                $hora = idate("H", $tiempo->time());
+
+                if ($dia == 0 || $hora >= 6 && $hora <= 22 || $dia == 6 && $hora >= 14 && $hora <= 22) {
+                    $trasbordo = true;
+                }
+            }
+
+            // Si es de noche (8pm a 5 am): tiempo máximo 120 minutos.
+            if ($tiempoTranscurrido <= 7200) {
+                $hora = idate("H", $tiempo->time());
+                if ($hora >= 20 || $hora <= 5) {
+                    $trasbordo = true;
+                }
+            }
+
+            if ($trasbordo) {
+                return TRUE;
+            } else {
+                return FALSE;
+            }
+        }
+    }
+
     public function descuentoSaldo(TiempoInterface $tiempo, ColectivoInterface $colectivo)
     {
-        if (BonificacionesTarjetas::trasbordo($tiempo, $colectivo)) {
+        if ($this->trasbordo($tiempo, $colectivo)) {
 
             $this->ultimopago = $tiempo->time();
             $this->lineaUltColectivo = $colectivo->linea();
