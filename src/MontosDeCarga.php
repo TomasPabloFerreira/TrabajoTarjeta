c109ea9de5362ba4d9d4a484b820cb2971385763
TomasPabloFerreira <tomasferreira373@gmail.com>
2019-12-03 03:48:50 -0300

    MontosDeCarga Updated

--- src/MontosDeCarga.php
+++ src/MontosDeCarga.php
@@ -9,6 +9,7 @@ class MontosDeCarga
         [ 'importe' => 20.0,    'recarga_adicional' => 0.0,     'acredita' => 20.0],
         [ 'importe' => 30.0,    'recarga_adicional' => 0.0,     'acredita' => 30.0],
         [ 'importe' => 50.0,    'recarga_adicional' => 0.0,     'acredita' => 50.0],
+        [ 'importe' => 100.0,   'recarga_adicional' => 0.0,     'acredita' => 100.0],
         [ 'importe' => 1119.90, 'recarga_adicional' => 180.10,  'acredita' => 1300.0],
         [ 'importe' => 2114.11, 'recarga_adicional' => 485.89,  'acredita' => 2600.0],
     ];
