9dff409a6e3d54480aee1cb3ab2c28d093c93ebc
TomasPabloFerreira <tomasferreira373@gmail.com>
2019-12-02 23:53:24 -0300

    Tarjeta recargar method restored

--- tests/TarjetaTest.php
+++ tests/TarjetaTest.php
@@ -61,7 +61,6 @@ class TarjetaTest extends TestCase {
       $this->assertTrue($tarjeta->recargar(30));
       $this->assertEquals($tarjeta->obtenerCantidadPlus(),2);
 
-
   }
 
       /**
@@ -72,15 +71,15 @@ class TarjetaTest extends TestCase {
   { $colectivo = new Colectivo(144,"RosarioBus",5);
     $tarjeta=new Tarjeta(2345);
     $tiempo = new TiempoFalso;
-//
-//    $this->assertTrue($colectivo->pagarCon($tiempo,$tarjeta));
-//    $this->assertTrue($colectivo->pagarCon($tiempo,$tarjeta));
-//    $this->assertFalse($colectivo->pagarCon($tiempo,$tarjeta));
+
+    $this->assertTrue($colectivo->pagarCon($tiempo,$tarjeta));
+    $this->assertTrue($colectivo->pagarCon($tiempo,$tarjeta));
+    $this->assertFalse($colectivo->pagarCon($tiempo,$tarjeta));
     $this->assertTrue($tarjeta->recargar(100));
-    $this->assertEquals($tarjeta->obtenerSaldo(), 100 /*- (2*Tarifas::BOLETO_NORMAL)*/ );
+    $this->assertEquals($tarjeta->obtenerSaldo(), 100 - (2*Tarifas::BOLETO_NORMAL) );
     $this->assertTrue($colectivo->pagarCon($tiempo,$tarjeta));
     $this->assertTrue($tarjeta->recargar(100));
-    $this->assertEquals($tarjeta->obtenerSaldo(), 100 + 100 - (/*3**/Tarifas::BOLETO_NORMAL) );
+    $this->assertEquals($tarjeta->obtenerSaldo(), 100 + 100 - (3*Tarifas::BOLETO_NORMAL) );
 
   }
  //Testeo que el trasbordo cumpla con los requisitos para funcionar.
