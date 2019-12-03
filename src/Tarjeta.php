e39127544155a38431d25c56809c95f6aa289156
TomasPabloFerreira <tomasferreira373@gmail.com>
2019-12-02 21:26:47 -0300

    Trasbordo nocturno

--- src/Tarjeta.php
+++ src/Tarjeta.php
@@ -60,14 +60,28 @@ class Tarjeta implements TarjetaInterface {
       if ($this->lineaUltColectivo != $colectivo->linea() && $this->cantTrasb == 0)
       {
           $trasbordo = false;
-          if ($tiempo->time() - $this->ultimopago <= 3600) {
+          $tiempoTranscurrido = $tiempo->time() - $this->ultimopago;
+
+          // Tiempo máximo 60 minutos.
+          if ( $tiempoTranscurrido <= 3600) {
               $trasbordo = true;
           }
-          if ($tiempo->time() - $this->ultimopago <= 5400) {
+
+          // Sábados de las 14 a 22 hs, domingos y feriados de 6 a 22 hs: tiempo máximo 90 minutos.
+          if ( $tiempoTranscurrido <= 5400) {
               $dia = date("l", $tiempo->time());
               $hora = idate("H", $tiempo->time());
+
               if ($dia == 0 || $hora >= 6 && $hora <= 22 || $dia == 6 && $hora >= 14 && $hora <= 22) {
                 $trasbordo = true;
+              }
+          }
+
+          // Si es de noche (8pm a 5 am): tiempo máximo 120 minutos.
+          if ( $tiempoTranscurrido <= 7200 ) {
+              $hora = idate("H", $tiempo->time());
+              if ( $hora >= 20 || $hora <= 5 ) {
+                  $trasbordo = true;
               }
           }
 
